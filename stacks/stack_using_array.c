ca9e76e98990b53821b35346f04ed799ce4b7086
Avinash <asdhakne21@gmail.com>
2022-05-06 20:44:59 +0530

    stack using array pointer

--- stacks/stack_using_array.c
+++ stacks/stack_using_array.c
@@ -6,41 +6,37 @@ struct stack
     int size;
     int top;
     int *arr;
-}s;
+};
 
 int isEmpty(struct stack *ptr)
 {
     if (ptr->top == -1)
     {
-        printf("stack is empty");
+        return 1;
     }
-    printf("stack is not empty");
-
+    return 0;
 }
 
-void isFull(struct stack *ptr)
+int isFull(struct stack *ptr)
 {
     if (ptr->top == ptr->size - 1)
     {
-        printf("stack is full");
+        return 1;
     }
-    printf("stack is not full");
-
+    return 0;
 }
 
 int main()
 {
 
-    struct stack s;
+    struct stack *s = (struct stack *)malloc(sizeof(struct stack *));
+    s->size = 50;
+    s->top = -1;
+    s->arr = (int *)malloc(s->size * sizeof(int));
 
-    s.size = 50;
-    s.top = -1;
-    s.arr = (int *) malloc(s.size * sizeof(int));
+    printf("%d \n", isEmpty(s));
 
-    isEmpty(&s);
-
-    s.top = 0;
-    s.arr[s.top] = 25;
-
-    isEmpty(&s);
+    s->top = 0;
+    s->arr[s->top] = 25;
+    printf("%d \n", isEmpty(s));
 }
