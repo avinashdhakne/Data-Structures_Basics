56a06bc22d228c6d0b1f52ca10acc4f71c43f4b7
Avinash <asdhakne21@gmail.com>
2022-05-17 05:54:15 +0530

    mirror image of BST

---
+++ Trees/Class_work/BST/BST_mirror_image.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createnode(int data)
+{
+    struct node *temp = (struct node *)malloc(sizeof(struct node));
+    temp->data = data;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+struct node *insert(struct node *root, int val)
+{
+    if (root == NULL)
+    {
+        root = createnode(val);
+    }
+
+    else if (root->data > val)
+    {
+        root->left = insert(root->left, val);
+    }
+    else
+    {
+        root->right = insert(root->right, val);
+    }
+
+    return root;
+}
+
+void mirrorImage(struct node *root)
+{
+    struct node *temp;
+    temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+
+    mirrorImage(root->left);
+    mirrorImage(root->right);
+
+    if(root == NULL){
+        return;
+    }
+}
+int main()
+{
+    struct node *root = NULL;
+    root = insert(root, 15);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    return 0;
+}
